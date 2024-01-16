b4e2241f3a7e2714bb2e9797130e3b187d0342a7
Jordan Falcão <jordanfalcaocs@gmail.com>
2024-01-16 18:32:33 -0300

    Adding new general way to solve the problem

--- treasure-map-challenge.py
+++ treasure-map-challenge.py
@@ -30,7 +30,33 @@ else:
 
 # setting the chosen index
 map[first_index][second_index] = 'X'
-# print(map)
+
+# Write your code above this row 👆
+# 🚨 Don't change the code below 👇
+print(f"{line1}\n{line2}\n{line3}")
+
+###################################################################3
+# ALTERNATIVE WAY
+
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+# 🚨 Don't change the code above 👆
+# Write your code below this row 👇
+
+# setting the NUMBER index
+number_index = int(position[1]) -1
+
+# setting the LETTER index
+letter = position[0].lower()        # lower to get general cases
+abc = ['a', 'b', 'c']               # 'abc' list to compare the letters
+letter_index = abc.index(letter)    # seeting the letter index according to 'abc' list and the input given
+
+# setting the chosen index: observe NUMBER is the first index and LETTER is the second one
+map[number_index][letter_index] = 'X'
 
 # Write your code above this row 👆
 # 🚨 Don't change the code below 👇
