76ad26f6f53876d89694f81b07fa27423a676008
Jordan Falcão <jordanfalcaocs@gmail.com>
2024-03-02 12:38:18 -0300

    Sent email in a specific day of the week

--- day-32/smtp-email/main.py
+++ day-32/smtp-email/main.py
@@ -1,8 +1,9 @@
-# import smtplib
-#
-# my_email = "jordan.python.bootcamp@gmail.com"
-# password = "usemyapppassword"  # CHANGE TO THE APP PASSWORD HERE
-#
+import smtplib
+import random
+
+my_email = "jordan.python.bootcamp@gmail.com"
+password = "apppasswordhere"  # CHANGE TO THE APP PASSWORD HERE  <<---------------
+
 # # create  SMTP object
 # with smtplib.SMTP('smtp.gmail.com') as connection:  # for gmail
 #     connection.starttls()  # Transport Layer Security: encript message
@@ -11,23 +12,40 @@
 #         from_addr=my_email, to_addrs="jordan.python@yahoo.com",
 #         msg="Subject: Email with Python\n\nHello, this is a Python message."
 #     )
-# # connection.close()  # avoid use it by usgin 'with' key
+# # connection.close()  # avoid use it by using 'with' key
 
 
 # ----------------------------------------DATETIME---------------------------------------- #
 import datetime as dt
 
-now = dt.datetime.now()  # return 'year-month-day hh:mm:ss.accacc'
+# now = dt.datetime.now()  # return 'year-month-day hh:mm:ss.accacc'
+#
+# # now.year
+# # now.month
+# # now.day
+# # now.hour
+# # now.minute
+# # now.second
+# day_of_week = now.weekday()
+# print(day_of_week)
+#
+# # create my own datetime object
+# date_of_birth = dt.datetime(year=1991, month=10, day=28)
+# print(date_of_birth)
 
-# now.year
-# now.month
-# now.day
-# now.hour
-# now.minute
-# now.second
-day_of_week = now.weekday()
-print(day_of_week)
+# -------------------------------SEND EMAIL IN A SPECIFIC DAY OF THE WEEK------------------------------- #
+today = dt.datetime.now()
 
-# create my own datetime object
-date_of_birth = dt.datetime(year=1991, month=10, day=28)
-print(date_of_birth)
+with open("quotes.txt", "r") as file:
+    quotes = file.readlines()  # create a list of all quotes
+
+# choose a specific day of the week
+if today.weekday() == 5:
+    message = random.choice(quotes)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email, to_addrs="jordan.python@yahoo.com",
+            msg=f"Subject:Monday quote\n\n{message}"
+        )
