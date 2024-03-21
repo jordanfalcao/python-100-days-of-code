f370d1dbd25a3e3c5c96975b8e6d389a09017f47
Jordan Falcão <jordanfalcaocs@gmail.com>
2024-03-20 21:22:44 -0300

    Final version: Env Variables, Sheety API and Nutrionix API

--- day-38/workout-tracking-project/main.py
+++ day-38/workout-tracking-project/main.py
@@ -1,15 +1,25 @@
 import requests
+from datetime import datetime
+import os
 
-APP_ID = "your app id"  # SET YOUR APP ID HERE
-API_KEY = "you app key"  # SET YOUR APP KEY HERE
-ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+GENDER = "male"
+WEIGHT_KG = 76
+HEIGHT_CM = 171
+AGE = 32
+EXERCISE_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+# environments variables
+APP_ID = os.environ["APP_ID"]
+API_KEY = os.environ["API_KEY"]
+SHEETY_ENDPOINT = os.environ["SHEETY_ENDPOINT"]
+BEARER_TOKEN = os.environ["BEARER_TOKEN"]
 
 parameters = {
     "query": input("Which exercise did you do today: "),
-    "gender": "male",
-    "weight_kg": 76,
-    "height_cm": 171,
-    "age": 32
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
 }
 
 header_parameters = {
@@ -17,6 +27,31 @@ header_parameters = {
     "x-app-key": API_KEY
 }
 
-response = requests.post(url=ENDPOINT, headers=header_parameters, json=parameters)
-result = response.json()
-print(result)
+# post the exercise with NLP
+response = requests.post(url=EXERCISE_ENDPOINT, headers=header_parameters, json=parameters)
+results = response.json()
+
+#####################################################
+headers_sheety = {
+    "Authorization": f"Bearer {BEARER_TOKEN}"
+}
+
+# loop to set all the exercises day
+for exercise in results['exercises']:
+    today = datetime.now()
+    formatted_hour = today.strftime("%X")
+    formatted_date = today.strftime("%m/%d/%Y")
+
+    # inputs to set in google sheet in json
+    sheet_inputs = {
+        "workout": {
+            "date": formatted_date,
+            "time": formatted_hour,
+            "exercise": exercise['name'].title(),
+            "duration": exercise['duration_min'],
+            "calories": exercise['nf_calories']
+        }
+    }
+
+    # set in the Google sheet with Sheety
+    sheet_response = requests.post(url=SHEETY_ENDPOINT, json=sheet_inputs, headers=headers_sheety)
